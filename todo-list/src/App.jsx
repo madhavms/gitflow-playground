8f1034a25ce9dbe786a1b676e7e3fa6ad2930847
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-06-16 13:44:48 +0100

    Implement todo-list feature

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from 'react'
+import './App.css'
+import TodoList from './components/TodoList'
+
+function App() {
+
+  return (
+    <Fragment>
+      <div>
+      <TodoList/>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ todo-list/src/components/TodoList.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      const newTodo = {
+        id: Date.now(),
+        text: inputValue,
+      };
+
+      setTodos([...todos, newTodo]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter a new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
